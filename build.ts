2859283e123da8e1c33c092012f6a5175320f64d
paulgo <paulg0@protonmail.com>
2022-06-27 20:01:34 -0400

    Add node-fetch dev dependency for build script

--- build.ts
+++ build.ts
@@ -1,31 +1,36 @@
 
-const Fs = require("fs") as typeof import("fs");
-const Path = require("path") as typeof import("path");
-const Proc = require("child_process") as typeof import("child_process");
-const Terser = require("terser") as typeof import("terser");
-//const fetch = require("node-fetch") as typeof window.fetch;
-
-/** */
-function copyResources()
 {
-	const files = [
-		"loki-incremental-indexeddb-adapter.js",
-		"loki-incremental-adapter.js",
-		"loki-indexed-adapter.js",
-		"loki.js",
-		"index.html",
-	];
-	
-	for (const fileName of files)
-		Fs.copyFileSync("./lib/" + fileName, "./build/" + fileName);
-}
+	const Fs = require("fs") as typeof import("fs");
+	const Path = require("path") as typeof import("path");
+	const Proc = require("child_process") as typeof import("child_process");
+	const Terser = require("terser") as typeof import("terser");
+	const fetch = require("node-fetch") as typeof window.fetch;
 
-/** */
-function deploy()
-{
-	copyResources();
-	
-	
-}
+	/** */
+	function copyResources()
+	{
+		const files = [
+			"loki-incremental-indexeddb-adapter.js",
+			"loki-incremental-adapter.js",
+			"loki-indexed-adapter.js",
+			"loki.js",
+			"medium-editor.default.min.css",
+			"medium-editor.min.css",
+			"medium-editor.min.js",
+			"index.html",
+		];
+		
+		for (const fileName of files)
+			Fs.copyFileSync("./lib/" + fileName, "./build/" + fileName);
+	}
 
-eval(process.argv.at(-1) || "");
+	/** */
+	function deploy()
+	{
+		copyResources();
+		
+		
+	}
+	
+	eval(process.argv.at(-1) || "");
+}
