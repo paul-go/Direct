051f969ed87e1a226e53070cfb00dd8a85ffdaaa
paulgo <paulg0@protonmail.com>
2022-07-23 17:58:01 -0400

    Adds the "Not" assertion library

---
+++ lib/Not.ts
@@ -0,0 +1,55 @@
+
+
+/**
+ * Utility class for performing basic guarding.
+ */
+namespace Not
+{
+	/**
+	 * @returns The argument as specified, but throws an
+	 * exception in the case when it's null or undefined.
+	 */
+	export function nullable<T>(param: T)
+	{
+		if (param === null || param === undefined)
+		{
+			debugger;
+			throw new ReferenceError();
+		}
+		
+		return param as NonNullable<T>;
+	}
+	
+	/**
+	 * @returns The argument as specified, but throws an
+	 * exception in the case when the value provided equates
+	 * to a JavaScript-falsey value.
+	 */
+	export function falsey<T>(value: T): NonNullable<T>
+	{
+		if (!value)
+		{
+			debugger;
+			throw new TypeError();
+		}
+		
+		return value as any;
+	}
+	
+	/**
+	 * Used to mark out areas of the code that are not implemented.
+	 */
+	export function implemented()
+	{
+		debugger;
+	}
+	
+	/**
+	 * 
+	 */
+	export function reachable()
+	{
+		debugger;
+		throw new TypeError();
+	}
+}
