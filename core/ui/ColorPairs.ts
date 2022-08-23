d6f33c5121d26345d24522fdd1d43c07a5d290ac
paulgo <paulg0@protonmail.com>
2022-08-23 08:11:38 -0400

    Adds more color pairs

--- core/ui/ColorPairs.ts
+++ core/ui/ColorPairs.ts
@@ -7,8 +7,11 @@ namespace App
 	 * dark color, and the second tuple is the light color.
 	 */
 	export const colorPairs: readonly TColorPair[] = [
+		[[0, 0, 35], [0, 0, 75]],
 		[[30, 85, 45], [30, 100, 70]],
 		[[215, 85, 45], [215, 100, 78]],
 		[[275, 85, 45], [275, 100, 80]],
+		[[315, 85, 45], [315, 100, 80]],
+		[[345, 85, 45], [345, 100, 80]],
 	];
 }
