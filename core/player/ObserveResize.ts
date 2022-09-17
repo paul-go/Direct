e339574bd59b7c814852b43c3e7071594ac75ea8
paulgo <paulg0@protonmail.com>
2022-09-17 10:26:07 -0400

    Fixes bugs in the ResizeObserver abstraction

--- core/player/ObserveResize.ts
+++ core/player/ObserveResize.ts
@@ -1,23 +1,48 @@
 
 namespace Player
 {
-	/** */
+	/**
+	 * Observes the resizing of the particular element, and invokes
+	 * the specified callback when the element is resized.
+	 */
 	export function observeResize(
 		e: HTMLElement,
-		callback: (rect: DOMRectReadOnly) => void)
+		callback: (width: number, height: number) => void)
 	{
 		if (typeof ResizeObserver !== "undefined")
 		{
 			new ResizeObserver(rec =>
 			{
-				if (rec.length > 0)
-					callback(rec[0].contentRect);
-			
-			}).observe(e);
+				if (rec.length === 0)
+					return;
+				
+				const entry = rec[0];
+				if (entry.borderBoxSize?.length > 0)
+				{
+					const size = entry.borderBoxSize[0];
+					callback(size.inlineSize, size.blockSize);
+				}
+				else
+				{
+					const width = e.offsetWidth;
+					const height = e.offsetHeight;
+					callback(width, height);
+				}
+			}).observe(e, { box: "border-box" });
 		}
 		else
 		{
-			// Implement
+			Hot.get(e)(
+				Hot.on(window, "resize", () =>
+				{
+					window.requestAnimationFrame(() =>
+					{
+						const width = e.offsetWidth;
+						const height = e.offsetHeight;
+						callback(width, height);
+					});
+				})
+			);
 		}
 	}
 }
